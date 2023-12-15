20395657c8816ffacc639ba56817cea5f9c60584
tan-sd <shengdatan@gmail.com>
2023-12-15 23:41:27 +0800

    Reset Player Ball When Dropped

--- src/Player.jsx
+++ src/Player.jsx
@@ -3,6 +3,7 @@ import { useFrame } from "@react-three/fiber";
 import { useRapier, RigidBody } from "@react-three/rapier";
 import { useState, useEffect, useRef } from "react";
 import * as THREE from "three";
+import useGame from "./stores/useGame";
 
 export default function Player() {
     const [subscribeKeys, getKeys] = useKeyboardControls();
@@ -12,6 +13,16 @@ export default function Player() {
         () => new THREE.Vector3(10, 10, 10)
     );
     const [smoothedCameraTarget] = useState(() => new THREE.Vector3());
+    const start = useGame((state) => state.start);
+    const end = useGame((state) => state.end);
+    const restart = useGame((state) => state.restart);
+    const blocksCount = useGame((state) => state.blocksCount);
+
+    const reset = () => {
+        body.current.setTranslation({ x: 0, y: 1, z: 0 });
+        body.current.setLinvel({ x: 0, y: 0, z: 0 });
+        body.current.setAngvel({ x: 0, y: 0, z: 0 });
+    };
 
     const jump = () => {
         const origin = body.current.translation();
@@ -26,23 +37,37 @@ export default function Player() {
     };
 
     useEffect(() => {
+        const unsubscribeReset = useGame.subscribe(
+            (state) => state.phase,
+            (value) => {
+                if (value === "ready") reset();
+            }
+        );
+
+        return () => {
+            unsubscribeReset();
+        };
+    });
+
+    useEffect(() => {
         const unsubscribeJump = subscribeKeys(
             (state) => state.jump,
             (value) => {
                 if (value) jump();
             }
         );
-        // subscribeKeys(
-        //     (state) => state.jump,
-        //     (value) => {
-        //         if (value) {
-        //             jump();
-        //         }
-        //     }
-        // );
+
+        const unsubscribeAny = subscribeKeys(() => {
+            start();
+        });
+
+        subscribeKeys(() => {
+            console.log("any key down");
+        });
 
         return () => {
             unsubscribeJump();
+            unsubscribeAny();
         };
     }, []);
 
@@ -63,6 +88,9 @@ export default function Player() {
 
         state.camera.position.copy(smoothedCameraPosition);
         state.camera.lookAt(smoothedCameraTarget);
+
+        if (bodyPosition.z < -(blocksCount * 4 + 2)) end();
+        if (bodyPosition.y < -4) restart();
     });
 
     useFrame((state, delta) => {
