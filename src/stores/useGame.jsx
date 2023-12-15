929ea35d8180e0f399331b6ef67b6fb48999e46c
tan-sd <shengdatan@gmail.com>
2023-12-15 23:41:02 +0800

    Add Global Variable and Function

---
+++ src/stores/useGame.jsx
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { subscribeWithSelector } from "zustand/middleware";
+
+export default create(
+    subscribeWithSelector((set) => {
+        return {
+            blocksCount: 5,
+
+            phase: "ready",
+
+            start: () => {
+                set((state) => {
+                    if (state.phase === "ready") return { phase: "playing" };
+
+                    return {};
+                });
+            },
+
+            restart: () => {
+                set((state) => {
+                    if (state.phase === "playing" || state.phase === "ended")
+                        return { phase: "ready" };
+
+                    return {};
+                });
+            },
+
+            end: () => {
+                set((state) => {
+                    if (state.phase === "playing") return { phase: "ended" };
+
+                    return {};
+                });
+            },
+        };
+    })
+);
