d411a05ed65c9783f7f638830af54da7e67b7092
tan-sd <shengdatan@gmail.com>
2023-12-17 22:29:42 +0800

    Add scoreboard and input field

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,6 +1,6 @@
 import { useKeyboardControls } from "@react-three/drei";
 import useGame from "./stores/useGame";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { addEffect } from "@react-three/fiber";
 
 export default function Interface() {
@@ -9,18 +9,30 @@ export default function Interface() {
     const leftward = useKeyboardControls((state) => state.leftward);
     const rightward = useKeyboardControls((state) => state.rightward);
     const jump = useKeyboardControls((state) => state.jump);
+    const enter = useKeyboardControls((state) => state.enter);
 
     const restart = useGame((state) => state.restart);
     const phase = useGame((state) => state.phase);
     const time = useRef();
 
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleReset = () => {
+        setInputValue(null);
+    };
+
     useEffect(() => {
         const unsubscribeEffect = addEffect(() => {
             const state = useGame.getState();
             let elapsedTime = 0;
-            if (state.phase === "playing")
+            if (state.phase === "playing") {
                 elapsedTime = Date.now() - state.startTime;
-            else if (state.phase === "ended")
+                handleReset();
+            } else if (state.phase === "ended")
                 elapsedTime = state.endTime - state.startTime;
             elapsedTime /= 1000;
             elapsedTime = elapsedTime.toFixed(2);
@@ -39,8 +51,51 @@ export default function Interface() {
                 0.00
             </div>
             {phase === "ended" && (
-                <div className="restart" onClick={restart}>
-                    Restart
+                <div className="scoreboard">
+                    <div className="title">LEADERBOARD</div>
+                    <div className="board">
+                        <div className="first">
+                            <div className="index first">1</div>
+                            <div className="name">KengBoon</div>
+                            <div className="score">6.50</div>
+                        </div>
+                        <div className="second">
+                            <div className="index second">2</div>
+                            <div className="name">Seth</div>
+                            <div className="score">6.50</div>
+                        </div>
+                        <div className="third">
+                            <div className="index third">3</div>
+                            <div className="name">Rachel</div>
+                            <div className="score">6.50</div>
+                        </div>
+                        <div className="divider">. . .</div>
+                        <div className="rest">
+                            <div className="index rest">4</div>
+                            <div className="inputCompartment">
+                                <input
+                                    className="name input"
+                                    placeholder="Enter your name"
+                                    maxLength="10"
+                                    value={inputValue}
+                                    onChange={handleInputChange}
+                                />
+                                {inputValue && (
+                                    <kbd
+                                        className={`enterBtn ${
+                                            enter ? "active" : ""
+                                        }`}
+                                    >
+                                        Enter
+                                    </kbd>
+                                )}
+                            </div>
+                            <div className="score">6.50</div>
+                        </div>
+                    </div>
+                    <div className="restart" onClick={restart}>
+                        Restart
+                    </div>
                 </div>
             )}
             {(phase === "playing" || phase === "ready") && (
