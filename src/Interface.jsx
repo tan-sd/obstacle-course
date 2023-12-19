04fdd2395b866e799fc7f003ba51e03d8d234985
tan-sd <shengdatan@gmail.com>
2023-12-19 23:33:55 +0800

    Implement backend logic and FontAwesome

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,7 +1,14 @@
 import { useKeyboardControls } from "@react-three/drei";
 import useGame from "./stores/useGame";
 import { useEffect, useRef, useState } from "react";
-import { addEffect } from "@react-three/fiber";
+import { addEffect, useFrame } from "@react-three/fiber";
+import { pushData, getData } from "./backend/firebase";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faSpinner,
+    faCheck,
+    faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Interface() {
     const forward = useKeyboardControls((state) => state.forward);
@@ -9,20 +16,55 @@ export default function Interface() {
     const leftward = useKeyboardControls((state) => state.leftward);
     const rightward = useKeyboardControls((state) => state.rightward);
     const jump = useKeyboardControls((state) => state.jump);
-    const enter = useKeyboardControls((state) => state.enter);
 
     const restart = useGame((state) => state.restart);
     const phase = useGame((state) => state.phase);
+    const register = useGame((state) => state.register);
+    const changeToRegister = useGame((state) => state.changeToRegister);
     const time = useRef();
 
+    const nickname = useGame((state) => state.nickname);
+
     const [inputValue, setInputValue] = useState("");
+
+    const [leaderboardData, setLeaderboardData] = useState([]);
+
+    const [entryPosition, setEntryPosition] = useState("");
+
+    const [pressSubmit, setPressSubmit] = useState(false);
+
+    const [submitting, setSubmitting] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
+    const [disableRestart, setDisableRestart] = useState(false);
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
 
     const handleReset = () => {
-        setInputValue(null);
+        setInputValue("");
+    };
+
+    const handlePush = async () => {
+        setSubmitting(true);
+        setDisableRestart(true);
+
+        const currentDate = new Date().toLocaleDateString();
+        const currentTime = new Date().toLocaleTimeString();
+
+        try {
+            await pushData(
+                nickname,
+                time.current.textContent,
+                currentDate,
+                currentTime
+            );
+            setSubmitted(true);
+        } catch (error) {
+            console.error("Error pushing data:", error);
+        } finally {
+            setSubmitting(false);
+        }
     };
 
     useEffect(() => {
@@ -40,60 +82,172 @@ export default function Interface() {
             if (time.current) time.current.textContent = elapsedTime;
         });
 
+        const fetchLeaderboardData = async () => {
+            var data = await getData();
+            if (data) {
+                const leaderboardEntries = Object.entries(data)
+                    .map(([key, entry]) => ({
+                        id: key,
+                        name: entry.name,
+                        score: parseFloat(entry.score),
+                    }))
+                    .sort((a, b) => a.score - b.score);
+                setLeaderboardData(leaderboardEntries);
+            }
+        };
+
+        fetchLeaderboardData();
+
         return () => {
             unsubscribeEffect();
         };
     }, []);
 
+    useEffect(() => {
+        if (phase === "ended") {
+            const newEntry = {
+                id: "newEntry",
+                name: nickname,
+                score: parseFloat(time.current.textContent),
+            };
+            leaderboardData[leaderboardData.length] = newEntry;
+
+            const leaderboardEntries = Object.entries(leaderboardData)
+                .map(([key, entry]) => ({
+                    id: key,
+                    name: entry.name,
+                    score: parseFloat(entry.score),
+                }))
+                .sort((a, b) => a.score - b.score);
+
+            const newEntryPosition = leaderboardEntries.findIndex(
+                (entry) =>
+                    entry.name === nickname &&
+                    entry.score === parseFloat(time.current.textContent)
+            );
+
+            if (newEntryPosition >= 3) {
+                leaderboardEntries[3] = newEntry;
+            }
+
+            setEntryPosition(newEntryPosition);
+
+            setLeaderboardData(leaderboardEntries);
+        }
+    }, [phase]);
+
     return (
         <div className="interface">
-            <div ref={time} className="time">
-                0.00
-            </div>
+            {phase === "ready" && (
+                <div ref={time} className="time">
+                    0.00
+                </div>
+            )}
+            {phase === "register" && (
+                <div className="register">
+                    <input
+                        className="registerInput"
+                        type="text"
+                        placeholder="Nickname"
+                        maxLength={10}
+                        value={inputValue}
+                        onChange={handleInputChange}
+                    />
+                    <button
+                        className="registerBtn"
+                        onClick={() => register(inputValue)}
+                        disabled={!inputValue.trim()}
+                    >
+                        Let's go!
+                    </button>
+                </div>
+            )}
             {phase === "ended" && (
                 <div className="scoreboard">
                     <div className="title">LEADERBOARD</div>
                     <div className="board">
-                        <div className="first">
-                            <div className="index first">1</div>
-                            <div className="name">KengBoon</div>
-                            <div className="score">6.50</div>
-                        </div>
-                        <div className="second">
-                            <div className="index second">2</div>
-                            <div className="name">Seth</div>
-                            <div className="score">6.50</div>
-                        </div>
-                        <div className="third">
-                            <div className="index third">3</div>
-                            <div className="name">Rachel</div>
-                            <div className="score">6.50</div>
-                        </div>
-                        <div className="divider">. . .</div>
-                        <div className="rest">
-                            <div className="index rest">4</div>
-                            <div className="inputCompartment">
-                                <input
-                                    className="name input"
-                                    placeholder="Enter your name"
-                                    maxLength="10"
-                                    value={inputValue}
-                                    onChange={handleInputChange}
-                                />
-                                {inputValue && (
-                                    <kbd
-                                        className={`enterBtn ${
-                                            enter ? "active" : ""
-                                        }`}
-                                    >
-                                        Enter
-                                    </kbd>
-                                )}
+                        {leaderboardData.slice(0, 3).map((entry, index) => (
+                            <div
+                                key={entry.id}
+                                className={`entry ${
+                                    index == 0 && entryPosition == 0
+                                        ? "highlighted"
+                                        : index == 1 && entryPosition == 1
+                                        ? "highlighted"
+                                        : index == 2 && entryPosition == 2
+                                        ? "highlighted"
+                                        : ""
+                                }`}
+                            >
+                                <div
+                                    className={`index ${
+                                        index === 0
+                                            ? "first"
+                                            : "" || index === 1
+                                            ? "second"
+                                            : "" || index === 2
+                                            ? "third"
+                                            : ""
+                                    }`}
+                                >
+                                    {index + 1}
+                                </div>
+                                <div className="name">{entry.name}</div>
+                                <div className="score">{entry.score}</div>
                             </div>
-                            <div className="score">6.50</div>
-                        </div>
+                        ))}
+                        <div className="divider">. . .</div>
+                        {leaderboardData.slice(3, 4).map((entry) => (
+                            <div
+                                key={entry.id}
+                                className={`entry ${
+                                    entryPosition >= 3 ? "highlighted" : ""
+                                }`}
+                            >
+                                <div className={`index`}>
+                                    {entryPosition >= 3
+                                        ? parseInt(entryPosition) + 1
+                                        : 4}
+                                </div>
+                                <div className="name">{entry.name}</div>
+                                <div className="score">{entry.score}</div>
+                            </div>
+                        ))}
                     </div>
-                    <div className="restart" onClick={restart}>
+                    <div
+                        className={`submit ${
+                            submitting || submitted ? "disabled" : ""
+                        }`}
+                        onClick={() => {
+                            handlePush();
+                            setPressSubmit(true);
+                        }}
+                    >
+                        {submitting && (
+                            <FontAwesomeIcon icon={faSpinner} spin />
+                        )}
+                        {!submitting && submitted && (
+                            <FontAwesomeIcon icon={faCheck} />
+                        )}
+                        {!submitting && !submitted && "Submit"}
+                    </div>
+                    <div
+                        className="restart"
+                        onClick={() => {
+                            restart();
+                            setSubmitting(false);
+                            setSubmitted(false);
+                            setDisableRestart(false);
+                            setEntryPosition("");
+                            if (pressSubmit === false) {
+                                leaderboardData.splice(
+                                    parseInt(entryPosition),
+                                    1
+                                );
+                            }
+                            setPressSubmit(false);
+                        }}
+                    >
                         Restart
                     </div>
                 </div>
@@ -119,6 +273,31 @@ export default function Interface() {
                             className={`key large ${jump ? "active" : ""}`}
                         ></div>
                     </div>
+                </div>
+            )}
+            {phase !== "register" && (
+                <div className="nicknameContainer">
+                    <div className="nickname">Playing as {nickname}</div>
+                    {(phase === "ended" || phase === "ready") && (
+                        <div
+                            className="changeNickname"
+                            onClick={() => {
+                                changeToRegister();
+                                setSubmitting(false);
+                                setSubmitted(false);
+                                setDisableRestart(false);
+                                if (pressSubmit === false) {
+                                    leaderboardData.splice(
+                                        parseInt(entryPosition),
+                                        1
+                                    );
+                                }
+                                setPressSubmit(false);
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faPenToSquare} />
+                        </div>
+                    )}
                 </div>
             )}
         </div>
